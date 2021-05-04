737e7ef0e4d5de6706f5cf6f39a21bb2d47daaed
Gal-Nelken <galnelken07@gmail.com>
2021-05-04 13:44:08 +0300

    header v1.2

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -3,17 +3,17 @@ export class AppHeader extends React.Component {
 
     render() {
         return (
-            <nav className="app-header ">
+            <nav className="app-header">
 <div className="logo-container">
 <img className="logo-img" src="./assets/img/logo.png" alt=""/>
 <h3>AppSuss</h3>
 </div>
                 <ul className="clean-list header-links">
+                    <li><NavLink exact to="/">Home</NavLink></li>
                     <li><NavLink to="/book">Book</NavLink></li>
                     <li><NavLink to="/keep">Kepp</NavLink></li>
                     <li><NavLink to="/mail">Mail</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink exact to="/">Home</NavLink></li>
 
                     {/* TODO: make button that open the links in a row */}
                     {/* <button>🔢</button> */}
