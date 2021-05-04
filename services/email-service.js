d0c88abf9d32e7302b78f48a590c16a72862e7ab
barleviatias <barlevi.atias@gmail.com>
2021-05-04 11:32:19 +0300

    basic setup

--- services/email-service.js
+++ services/email-service.js
@@ -2,6 +2,11 @@
 export const emailService = {
     query
 }
+gEmails=[
+    {subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594},
+    {subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594},
+    {subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594}
+]
 
 function query() {
     return Promise.resolve(items)
