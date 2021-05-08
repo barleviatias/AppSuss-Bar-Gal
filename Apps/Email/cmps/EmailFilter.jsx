3ab38e81941b2db6d253137cf4f95f6b9c81f1cd
barleviatias <barlevi.atias@gmail.com>
2021-05-08 21:26:32 +0300

    design improve search

--- Apps/Email/cmps/EmailFilter.jsx
+++ Apps/Email/cmps/EmailFilter.jsx
@@ -1,38 +1,49 @@
 export class EmailFilter extends React.Component {
-    
-    state = {
-        filterBy: {
-            keyword: '',
-        }
-    }
+	state = {
+		filterBy: {
+			keyword: '',
+		},
+	};
 
-    handleChange = (ev) =>{
-        const field = ev.target.name
-        const value = ev.target.type === 'range' ? +ev.target.value : ev.target.value
-        this.setState({ filterBy: { ...this.state.filterBy, [field]: value }})
-    }
+	handleChange = (ev) => {
+		const field = ev.target.name;
+		const value =
+			ev.target.type === 'range' ? +ev.target.value : ev.target.value;
+		this.setState({ filterBy: { ...this.state.filterBy, [field]: value } });
+	};
 
-    onFilter = (ev) => {
-        ev.preventDefault()
-        const {filterBy} = this.state
+	onFilter = (ev) => {
+		ev.preventDefault();
+		const { filterBy } = this.state;
 
-        console.log(this.props);
-        console.log(filterBy);
-        this.props.onSetFilter(filterBy)
-    }
+		console.log(this.props);
+		console.log(filterBy);
+		this.props.onSetFilter(filterBy);
+	};
 
-    render() {
-        const { keyword, isRead } = this.state.filterBy
-        return (
-            <form className="email-filter" onSubmit={this.onFilter}>
-                <label htmlFor="byName">search</label>
-                <input type="text" id="byName" placeholder="search" name="keyword" value={keyword} onChange={this.handleChange} />
-                {/* <label htmlFor="byPriceRange">Price range</label>
+	render() {
+		const { keyword, isRead } = this.state.filterBy;
+		return (
+            <section className="email-search">
+
+			<form className="email-filter" onSubmit={this.onFilter}>
+				
+					<span class="material-icons">search</span>
+				<input
+					type="text"
+					id="byName"
+					placeholder="search"
+					name="keyword"
+					value={keyword}
+					onChange={this.handleChange}
+                    />
+				{/* <label htmlFor="byPriceRange">Price range</label>
                 <input type="range" id="byPriceRange" name="maxPriceRange" min={10} max={200} value={maxPriceRange} onChange={this.handleChange}/>
-                <span>{`10 - ${maxPriceRange}`}</span> */}
-                
-                <button>Apply Changes</button>
-            </form>
-        )
-    }
+            <span>{`10 - ${maxPriceRange}`}</span> */}
+{/* 
+				<button>Apply Changes</button> */}
+			</form>
+                </section>
+		);
+	}
 }
